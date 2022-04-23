b2cd9c7b7f3eca4781a3b383572e0c6755a02a24
aymansainshy <aymansainshy@gmail.com>
2022-04-23 22:05:53 +0200

    Add Recursive Binary Search

--- 21 - Binary Search/binary_search.dart
+++ 21 - Binary Search/binary_search.dart
@@ -1,7 +1,7 @@
 void main() {
   List numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-  print(binarySearch(numbers, 13));
+  print(binarySearch2(numbers, 0, numbers.length - 1, 13));
 
   // binarySearch(numbers, 13);
 }
@@ -13,8 +13,6 @@ int binarySearch(List list, int value) {
   int middle = ((start + end) / 2).floor();
 
   while (list[middle] != value && start <= end) {
-    print("$start ---- $middle ---- $end");
-    print("${list[start]} --- ${list[middle]} --- ${list[end]}\n");
 
     if (value < list[middle]) {
       end = middle - 1;
@@ -26,5 +24,29 @@ int binarySearch(List list, int value) {
   }
 
   if (list[middle] == value) return middle;
+
   return -1;
 }
+
+
+
+// Recursive Binary Search ..................
+int binarySearch2(List list, int start, int end, int value) {
+
+  if (end >= start) {
+    int mid = ((start + end) / 2).floor();
+
+    if (list[mid] == value) return mid;
+
+    if (list[mid] > value) return binarySearch2(list, start, mid - 1, value);
+
+    return binarySearch2(list, mid + 1, end, value);
+  }
+
+  return -1;
+}
+
+
+
+// print("$start ---- $middle ---- $end");
+// print("${list[start]} --- ${list[middle]} --- ${list[end]}\n");
