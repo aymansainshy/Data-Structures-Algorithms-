ea92bbc6f21973042f6f6d08088fcf135c7b67d4
aymansainshy <aymansainshy@gmail.com>
2022-05-10 13:26:46 +0200

    Edit Some Code

--- 32 - Naive  Approach/naive_approach.dart
+++ 32 - Naive  Approach/naive_approach.dart
@@ -11,12 +11,12 @@ void naivApproach(String text, String pattern) {
   int textLength = text.length;
   int patternLength = pattern.length;
 
-  for (int i = 0; i < textLength - patternLength; i++) {
+  for (int i = 0; i <= textLength - patternLength; i++) {
     int j;
 
     for (j = 0; j < patternLength; j++) {
       print(text[i + j] + " " + pattern[j]);
-      
+
       if (text[i + j] != pattern[j]) {
         print("Break !");
         break;
